892f0e00cf0ba3a9291a9fabea2006135ad20c97
LocDayNha <vohoangloc200@gmail.com>
2023-07-21 19:39:39 +0700

    Mo rong

---
+++ app/src/main/java/com/example/myfpl/activity/FormActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfpl.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myfpl.R;
+
+public class FormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}
