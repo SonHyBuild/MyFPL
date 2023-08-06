fef2beb46041822134330f5d8f50e5dd45e2f9f0
DZViet <117284050+DZViet@users.noreply.github.com>
2023-08-06 14:30:39 +0700

    Login User

--- app/src/main/java/com/example/myfpl/services/APIService.java
+++ app/src/main/java/com/example/myfpl/services/APIService.java
@@ -4,11 +4,15 @@ import com.example.myfpl.model.Course;
 import com.example.myfpl.model.LichHoc;
 import com.example.myfpl.model.LichThi;
 import com.example.myfpl.model.TinTuc;
+import com.example.myfpl.model.User;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface APIService {
@@ -31,6 +35,12 @@ public interface APIService {
     Call<ArrayList<TinTuc>> GetDSTT();
     @GET("Course.php")
     Call<ArrayList<Course>> GetDSCourse();
+
+    @GET("Login.php")
+    Call<Void> Login(
+            @Query("email") String email,
+            @Query("password") String password
+            );
 
 
 
